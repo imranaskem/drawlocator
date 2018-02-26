e07edafd135e8e591d45ecc922db2e988dd227b7
Imran Askem <imran.askem@gmail.com>
2018-02-26 23:03:01 +0000

    Basic Twilio support

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -48,11 +49,29 @@ func (a *App) initialiseRoutes() {
 	a.Router.GET("/staff", a.getAllStaffLocations)
 	a.Router.OPTIONS("/staff", a.handleOptions)
 
+	a.Router.POST("/sms", a.smsHandler)
+
 	a.Router.GET("/staff/:id", a.getStaffLocation)
 	a.Router.PATCH("/staff/:id", a.updateStaffLocation)
 	a.Router.OPTIONS("/staff/:id", a.handleOptions)
 
 	a.Router.GET("/websocket", a.websocketHandler)
+}
+
+func (a *App) smsHandler(c *gin.Context) {
+	msg, _ := c.GetPostForm("Body")
+	from, _ := c.GetPostForm("From")
+
+	pers := a.findPersonbyPhoneNumber(from)
+
+	place := standardisePlace(msg)
+
+	pers.PlaceOfWork = place
+
+	a.updatePerson(pers)
+
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.String(http.StatusOK, "Location updated to "+place)
 }
 
 func (a *App) handleOptions(c *gin.Context) {
@@ -138,4 +157,46 @@ func originCheck(r *http.Request) bool {
 	} */
 
 	return true
+}
+
+func (a *App) findPersonbyID(id int) person {
+	var existingPerson person
+	_ = a.DB.C("people").Find(bson.M{"id": id}).One(&existingPerson)
+
+	return existingPerson
+}
+
+func (a *App) findPersonbyPhoneNumber(number string) person {
+	var existingPerson person
+	_ = a.DB.C("people").Find(bson.M{"phone": number}).One(&existingPerson)
+
+	return existingPerson
+}
+
+func (a *App) updatePerson(p person) error {
+	return a.DB.C("people").Update(bson.M{"id": p.ID}, &p)
+}
+
+func standardisePlace(place string) string {
+	switch {
+	case strings.Contains(place, "baker"):
+		return "Baker Street"
+
+	case strings.Contains(place, "sick"):
+		return "Sick"
+
+	case strings.Contains(place, "weston"):
+		return "Weston Street"
+
+	case strings.Contains(place, "holiday"):
+		return "Holiday"
+
+	case strings.Contains(place, "client"):
+		return "Client Office"
+
+	case strings.Contains(place, "home"):
+		return "Working from Home"
+	}
+
+	return place
 }
