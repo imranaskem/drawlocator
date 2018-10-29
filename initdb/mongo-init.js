96e3ada1e3c0518c80b86e62bd420ea2a06cb69b
Imran Askem <imran.askem@gmail.com>
2018-10-29 00:13:01 +0000

    Migrating Mongo to Docker

---
+++ initdb/mongo-init.js
@@ -0,0 +1,41 @@
+let res = [
+    db.createCollection("people"),
+    db.people.insert({
+        id: "1",
+        firstname: "Kent",
+        lastname: "Valentine",
+        placeofwork: "Holiday"
+    }),
+    db.people.insert({
+        id: "2",
+        firstname: "Dean",
+        lastname: "Faulkner",
+        placeofwork: "Holiday"
+    }),
+    db.people.insert({
+        id: "3",
+        firstname: "James",
+        lastname: "Nichols",
+        placeofwork: "Holiday"
+    }),
+    db.people.insert({
+        id: "4",
+        firstname: "Imran",
+        lastname: "Askem",
+        placeofwork: "Holiday"
+    }),
+    db.people.insert({
+        id: "5",
+        firstname: "Sian",
+        lastname: "Barlow",
+        placeofwork: "Holiday"
+    }),
+    db.people.insert({
+        id: "6",
+        firstname: "John",
+        lastname: "Sheard",
+        placeofwork: "Holiday"
+    })
+]
+
+printjson(res)
